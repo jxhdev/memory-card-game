76c4ad95e252a048ceb9d9e4cc6be87d95471be0
jxhdev <jxhdev@gmail.com>
2018-05-13 17:28:46 -0400

    refactoring flips

--- main.js
+++ main.js
@@ -5,38 +5,51 @@ window.onload = function() {
     let clicks = 0;
     let flipCount = 0;
     let flipOne;
-    cards.forEach(item => item.addEventListener('click', function () {
-        if(flipOne !== item) {
+    let gameOver = false;
+    let flipsRemaining = 12;
+    cards.forEach(item => item.addEventListener('click', function() {
+        flip(item)
+    }));
+
+
+
+function flip (item) {
+    // this is to prevent more than two flips at a time
+    if(flipCount < 2) {
+        if (flipOne !== item) {
             clicks += 1;
             count.textContent = clicks;
         }
-        
-
-        item.classList.toggle('is-flipped');
-
-        if(flipCount === 0) {
+        if (flipCount === 0) {
+            item.classList.toggle('is-flipped');
             flipOne = item
             flipCount++;
         } else {
-            if (flipOne.lastElementChild.lastElementChild.src === 
+            item.classList.toggle('is-flipped');
+            flipCount++;
+            if (flipOne.lastElementChild.lastElementChild.src ===
                 item.lastElementChild.lastElementChild.src) {
+                setTimeout(function () {
+                    flipOne.outerHTML = flipOne.outerHTML;
+                    item.outerHTML = item.outerHTML;
+                }, 1000);
                 flipCount = 0;
+                flipsRemaining -= 2;
             } else {
                 unflip(item);
             }
         }
 
+    }
+}
 
-    }));
-
-    
 function unflip(item) {
     console.log("Unflipping");
-    flipCount = 0;
     setTimeout(function () {
         flipOne.classList.toggle('is-flipped')
         item.classList.toggle('is-flipped')
     }, 1500)
+    flipCount = 0;
 }
 
 };
